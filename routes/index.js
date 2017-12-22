c21d02a11d29c4e2597fe10fa9fb1d5ac5c2bf7f
darwishdd <el7lisydarwish@gmail.com>
2017-12-23 00:49:53 +0200

    books table is ready

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,13 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/admin_panel', function(req, res, next) {
+    con.query("SELECT * FROM books", function (err, result, field) {
+        if (err) throw err;
+        res.render('admin_panel', { title: 'Express', books: result });
+    });
+});
+
 router.get('/add_book', function (req, res, next) {
    res.render("addbook", { title: 'Express'});
 });
@@ -25,6 +32,20 @@ router.post('/add_book', function (req, res, next) {
     con.query("INSERT INTO books SET ?", req.body, function (err, result, field) {
         if (err) throw err;
         res.render("addbook", { title: 'Express', bookAdded: true});
+    });
+});
+
+router.get('/prog_books', function(req, res, next) {
+    con.query("SELECT * FROM books WHERE category='programming'", function (err, result, field) {
+        if (err) throw err;
+        res.render('prog_books', { title: 'Express', books: result });
+    });
+});
+
+router.get('/lit_books', function(req, res, next) {
+    con.query("SELECT * FROM books WHERE category='literature'", function (err, result, field) {
+        if (err) throw err;
+        res.render('lit_books', { title: 'Express', books: result });
     });
 });
 
